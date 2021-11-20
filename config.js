255e248c4921d6f6324b7a04522381f78b16bc6d
Uzma18 <75409845+Uzma18@users.noreply.github.com>
2021-11-20 09:25:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCJIeGYtQHNgevR4Zdgw0dY45bXLqa3Dk",
+    authDomain: "myquizgame-23947.firebaseapp.com",
+    databaseURL: "https://myquizgame-23947-default-rtdb.firebaseio.com",
+    projectId: "myquizgame-23947",
+    storageBucket: "myquizgame-23947.appspot.com",
+    messagingSenderId: "1074944890350",
+    appId: "1:1074944890350:web:096cd6bf775cc02dd9aaff"
+  };
